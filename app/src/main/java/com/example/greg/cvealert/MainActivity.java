920911e19ac57c207bc17e0fd2310e5a4eca033a
Greg <beardygreg@gmail.com>
2015-04-06 19:45:10 +0100

    'Finished' RssTask Class, outputs ArrayList[Hashmap] of RSS items.

--- app/src/main/java/com/example/greg/cvealert/MainActivity.java
+++ app/src/main/java/com/example/greg/cvealert/MainActivity.java
@@ -16,6 +16,8 @@ import org.w3c.dom.NodeList;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -79,14 +81,16 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    public static class RssTask extends AsyncTask<Void,Void,Void>{
+    public static class RssTask extends AsyncTask<Void,Void,ArrayList<HashMap<String, String>>>{
     //Async class allows running in the background without stalling the GUI thread
 
         @Override
-        protected Void doInBackground(Void... params){
+        protected ArrayList<HashMap<String, String>> doInBackground(Void... params){
 
             //String defines location to look for RSS feed
             String downloadURL="http://www.cvedetails.com/vulnerability-feed.php?vendor_id=0&product_id=0&version_id=0&orderby=3&cvssscoremin=0";
+
+            ArrayList<HashMap<String,String>> results = new ArrayList<>();
 
             //Make GET request and create input stream from returned data
             try {
@@ -95,15 +99,20 @@ public class MainActivity extends ActionBarActivity {
                 connection.setRequestMethod(("GET"));
                 InputStream inputStream = connection.getInputStream();
                 //Call class to process the input stream as XML
-                processXML(inputStream);
+                results = processXML(inputStream);
             }catch (Exception e) {
                 //print exception if cannot fetch RSS from URL
                 Log.v("doInBackground", e + "");
             }
-            return null;
+            return results;
         }
 
-        public void processXML(InputStream inputStream) throws Exception {
+        @Override
+       protected void onPostExecute(ArrayList<HashMap<String, String>> result){
+            Log.v("OUTPUT", "" + result);
+        }
+
+        public ArrayList<HashMap<String,String>> processXML(InputStream inputStream) throws Exception {
 
             //Sets up the needed DocumentBuilder Factory and Object to parse the RSS Feed
 
@@ -112,27 +121,57 @@ public class MainActivity extends ActionBarActivity {
             Document xmlDocument = documentBuilder.parse(inputStream);
             Element rootElement = xmlDocument.getDocumentElement();
             Log.v("processXML", rootElement.getTagName());
+
             NodeList itemsList = rootElement.getElementsByTagName("item");
             NodeList itemChildren = null;
+
             Node currentItem = null;
             Node currentChild = null;
 
+            HashMap<String, String> currentMap = null;
+            ArrayList<HashMap<String, String>> results;
+            results = new ArrayList<>();
 
             //Loops through the XML tags printing the title contents of each item
             for(int i = 0; i < itemsList.getLength(); i++) {
 
+                currentMap = new HashMap<>();
+
                 currentItem = itemsList.item(i);
+
                 //Log.v("processXML", currentItem.getNodeName());
                 itemChildren = currentItem.getChildNodes();
 
                 for (int j = 0; j < itemChildren.getLength(); j++) {
-                    currentChild = itemChildren.item(j);
-                    if (currentChild.getNodeName().equalsIgnoreCase("title")){
-                        Log.v("processXML", currentChild.getTextContent());
-                    }
 
+                    currentChild = itemChildren.item(j);
+
+                    if (currentChild.getNodeName().equalsIgnoreCase("title")){
+//                        Log.v("processXML", currentChild.getTextContent());
+                          currentMap.put("title", currentChild.getTextContent());
+                    }
+                    if (currentChild.getNodeName().equalsIgnoreCase("description")){
+//                        Log.v("processXML", currentChild.getTextContent());
+                        currentMap.put("description", currentChild.getTextContent());
+                    }
+                    if (currentChild.getNodeName().equalsIgnoreCase("link")){
+//                        Log.v("processXML", currentChild.getTextContent());
+                        currentMap.put("link", currentChild.getTextContent());
+                    }
+                    if (currentChild.getNodeName().equalsIgnoreCase("pubDate")){
+//                        Log.v("processXML", currentChild.getTextContent());
+                        currentMap.put("pubDate", currentChild.getTextContent());
+                    }
                 }
+
+                if(currentMap != null && !currentMap.isEmpty()) {
+                    results.add(currentMap);
+//                    Log.v("inif", "adding results to array");
+                }
+
             }
+//            Log.v("Results:", "" + results);
+            return results;
 
         }
 
